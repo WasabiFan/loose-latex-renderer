4ae480d82f72a96de1abd032ec196fece9119363
WasabiFan <wasabifan@outlook.com>
2016-05-18 09:50:23 -0700

    Add logging

--- src/LatexRenderer.java
+++ src/LatexRenderer.java
@@ -3,6 +3,11 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.swing.JLabel;
 
@@ -13,16 +18,37 @@ import org.scilab.forge.jlatexmath.TeXIcon;
 public class LatexRenderer {
 
     public static void main(String[] args) {
+        try {
+            File logFile = new File("Log.txt");
+            logFile.createNewFile();
+            System.setOut(new PrintStream(new FileOutputStream(logFile.getAbsolutePath()), true));
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace(System.out);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace(System.out);
+        }
+        
+        System.out.println("New run ------------");
+        
         if(args.length < 1)
             throw new RuntimeException("Arguments missing! A string must be provided with the LaTeX string to render.");
         
+        System.out.println("Instantiating formula");
         TeXFormula formula = new TeXFormula(args[0]);
 
+        System.out.println("Creating icon");
         TeXIcon icon = formula.new TeXIconBuilder().setStyle(TeXConstants.STYLE_DISPLAY).setSize(50).build();
         icon.setInsets(new Insets(5, 5, 5, 5));
 
+        System.out.println("Copying to clipboard");
         ClipboardImageManager clipImg = new ClipboardImageManager(latexIconToImage(icon));
         clipImg.copyToClipboard();
+        
+        System.out.println("Done");
     }
 
     private static Image latexIconToImage(TeXIcon icon) {
